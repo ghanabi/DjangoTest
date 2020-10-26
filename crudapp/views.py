2ae45ac8eb7642654c0fdcaec7fc884104824c3b
jowanje <bluelove2521@gmail.com>
2020-10-26 11:19:39 +0900

    add paging function

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
+from .forms import BlogUpdate
 
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects.order_by('-id')
-    return render(request, 'home.html', {'blogs': blogs})
+    blog_list = Blog.objects.all().order_by('-id')
+    paginator = Paginator(blog_list,3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request,'home.html', {'blogs':blogs,'posts':posts})    
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
@@ -27,21 +34,24 @@ def postcreate(request):
 def update(request, blog_id):
     blog = Blog.objects.get(id=blog_id)
 
-    if request.method == "POST":
-        blog.title = request.POST['title']
-        blog.body = request.POST['body']
-        blog.pub_date = timezone.datetime.now()
-        blog.save()
-        return redirect('/crudapp/detail/' + str(blog.id))
-
+    if request.method =='POST':
+        form = BlogUpdate(request.POST)
+        if form.is_valid():
+            blog.title = form.cleaned_data['title']
+            blog.body = form.cleaned_data['body']
+            blog.pub_date=timezone.now()
+            blog.save()
+            return redirect('/crudapp/detail/' + str(blog.id))
     else:
-        return render(request, 'update.html')
+        form = BlogUpdate(instance = blog)
+ 
+        return render(request,'update.html', {'form':form})
 
 def delete(request, blog_id):
     blog = Blog.objects.get(id=blog_id)
     blog.delete()
     return redirect('/')
-    
+
 def new(request):
     full_text = request.GET['fulltext']
 
